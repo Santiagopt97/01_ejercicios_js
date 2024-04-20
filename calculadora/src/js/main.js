7cb549cd7bff9db3144271b63b6ce7efdc62dc01
Santiagopt97 <santiagopineda99@gmail.com>
2024-04-19 21:03:31 -0500

    calculadora lista

---
+++ calculadora/src/js/main.js
@@ -0,0 +1,9 @@
+const x = Number(prompt("Ingresa un numero porfi"))
+const y = Number(prompt("Ingresa otro numero porfi"))
+
+const suma = x+y
+const resta = x-y
+const multi = x*y
+const div = x/y
+
+console.info("La suma de los dos numeros", x,"y", y, " es:", suma, "la resta es:", resta, "la multiplicacion es: ", multi, "y la division es:", div)
